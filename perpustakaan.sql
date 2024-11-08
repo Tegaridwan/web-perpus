fe943a43c0a15e10713f0bdbedde5487e6bae7e6
Tegar <144606044+Tegaridwan@users.noreply.github.com>
2024-11-08 10:48:52 +0700

    Add files via upload

--- perpustakaan.sql
+++ perpustakaan.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 13, 2020 at 11:49 AM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.4.7
+-- Waktu pembuatan: 06 Nov 2024 pada 16.22
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_anggota`
+-- Struktur dari tabel `dd`
+--
+
+CREATE TABLE `dd` (
+  `dd` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pinjaman`
+--
+
+CREATE TABLE `pinjaman` (
+  `id` int(11) NOT NULL,
+  `judul_buku` varchar(200) NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `id_user` int(200) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pinjaman`
+--
+
+INSERT INTO `pinjaman` (`id`, `judul_buku`, `foto`, `id_user`, `nama`) VALUES
+(13, 'prabowo sang pemersatu bangsa', '672b09899b4fa.jpg', 9, 'neymar'),
+(14, 'jokowi pemimpin rakyat ', '6729b90606047.jpg', 9, 'neymar'),
+(15, 'prabowo sang pemersatu bangsa', '672b09899b4fa.jpg', 9, 'neymar');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_anggota`
 --
 
 CREATE TABLE `tb_anggota` (
@@ -35,10 +68,10 @@ CREATE TABLE `tb_anggota` (
   `tgl_lahir` date NOT NULL,
   `jk` enum('L','P') NOT NULL,
   `prodi` varchar(75) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tb_anggota`
+-- Dumping data untuk tabel `tb_anggota`
 --
 
 INSERT INTO `tb_anggota` (`id_anggota`, `nim`, `nama_anggota`, `tempat_lahir`, `tgl_lahir`, `jk`, `prodi`) VALUES
@@ -48,7 +81,7 @@ INSERT INTO `tb_anggota` (`id_anggota`, `nim`, `nama_anggota`, `tempat_lahir`, `
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_buku`
+-- Struktur dari tabel `tb_buku`
 --
 
 CREATE TABLE `tb_buku` (
@@ -59,22 +92,27 @@ CREATE TABLE `tb_buku` (
   `tahun_terbit` varchar(4) NOT NULL,
   `isbn` varchar(25) NOT NULL,
   `jumlah_buku` int(3) NOT NULL,
+  `kategori` varchar(20) NOT NULL,
   `lokasi` enum('Rak 1','Rak 2','Rak 3') NOT NULL,
-  `tgl_input` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `tgl_input` date NOT NULL,
+  `foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tb_buku`
+-- Dumping data untuk tabel `tb_buku`
 --
 
-INSERT INTO `tb_buku` (`id_buku`, `judul_buku`, `pengarang_buku`, `penerbit_buku`, `tahun_terbit`, `isbn`, `jumlah_buku`, `lokasi`, `tgl_input`) VALUES
-(4, 'Belajar HTML', 'surya', 'Erlangga', '2018', '234312', 3, 'Rak 3', '2020-07-21'),
-(6, 'Belajar Codeigniter', 'ridho', 'budi store', '2019', '45234521', 10, 'Rak 2', '2020-07-27');
+INSERT INTO `tb_buku` (`id_buku`, `judul_buku`, `pengarang_buku`, `penerbit_buku`, `tahun_terbit`, `isbn`, `jumlah_buku`, `kategori`, `lokasi`, `tgl_input`, `foto`) VALUES
+(10, 'biografi soeharto', 'amien rais', 'erlangga', '2008', '', 5, '', 'Rak 1', '2024-11-04', '672872096a5dc.jpg'),
+(13, '10 dosa besar soeharto', 'megawati', 'dpr', '2007', '', 6, '', 'Rak 2', '2024-11-05', '67297f1cd8fcd.jpg'),
+(18, 'memahami china', 'ahok', 'erlangga', '2008', 'dawawawawawawawawawawawaw', 4, 'Cerpen', 'Rak 1', '2024-11-05', '672986e807b21.jpeg'),
+(19, 'jokowi pemimpin rakyat ', 'prabowo', 'kabinet merah putih', '1995', 'h;unjbbbbbbbbbbbbbbbbb', 5, 'Biografi', 'Rak 1', '2024-11-05', '6729b90606047.jpg'),
+(20, 'prabowo sang pemersatu bangsa', 'prabowo', 'kabinet merah putih', '2024', 'wdadadadadadadadadadadada', 5, 'Biografi', 'Rak 1', '2024-11-06', '672b09899b4fa.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_transaksi`
+-- Struktur dari tabel `tb_transaksi`
 --
 
 CREATE TABLE `tb_transaksi` (
@@ -85,21 +123,22 @@ CREATE TABLE `tb_transaksi` (
   `tgl_pinjam` varchar(50) NOT NULL,
   `tgl_kembali` varchar(50) NOT NULL,
   `status` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tb_transaksi`
+-- Dumping data untuk tabel `tb_transaksi`
 --
 
 INSERT INTO `tb_transaksi` (`id_transaksi`, `id_buku`, `nim_transaksi`, `id_anggota`, `tgl_pinjam`, `tgl_kembali`, `status`) VALUES
 (7, 4, 2, 2, '01-07-2020', '23-07-2020', 'kembali'),
 (8, 6, 3, 3, '01-07-2020', '5-07-2020', 'pinjam'),
-(11, 4, 3, 3, '13-07-2020', '20-07-2020', 'pinjam');
+(11, 4, 3, 3, '13-07-2020', '20-07-2020', 'pinjam'),
+(12, 13, 3, 3, '06-11-2024', '13-11-2024', 'pinjam');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_user`
+-- Struktur dari tabel `tb_user`
 --
 
 CREATE TABLE `tb_user` (
@@ -107,71 +146,88 @@ CREATE TABLE `tb_user` (
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `nama` varchar(50) NOT NULL,
-  `foto` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `level` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tb_user`
+-- Dumping data untuk tabel `tb_user`
 --
 
-INSERT INTO `tb_user` (`id_user`, `username`, `password`, `nama`, `foto`) VALUES
-(2, 'harun', '$2y$10$0ytYJF98cGtxAe88cu4B3ekzNYHA1ViwnKGXSDWp00gDwykd.og8.', 'harun', '5f0b28b0c14b3.jpg');
+INSERT INTO `tb_user` (`id_user`, `username`, `password`, `nama`, `level`) VALUES
+(2, 'harun', '$2y$10$0ytYJF98cGtxAe88cu4B3ekzNYHA1ViwnKGXSDWp00gDwykd.og8.', 'harun', ''),
+(4, 'ronaldo07', '$2y$10$g/dc238vxatCSdw/7T.8Rekiq9qFAlcKIIwTFIfWEHoRtUnyhoZDq', 'ronaldo', 'admin'),
+(9, 'neymar77', '$2y$10$YQ6fp0d6fNnX3ywEOA28puUBVxFTxZ6qhGzUQi.nP9ZIfGhY3/ANC', 'neymar', 'peminjam'),
+(10, 'petugas1', '$2y$10$LsKuAnw.p9ELS2jhIF0jIeAa29A3PIIeRptPuS4TC2KCzioxrxzWy', 'petugas1', 'petugas'),
+(11, 'petugas2', '$2y$10$/rzVQ2q2Bj..7BWAlykGsuZpGHjIDKsdDAe6Y/T9lj8XOFzd4/nI6', 'petugas2', 'petugas'),
+(13, 'messi10', '$2y$10$HSbkS3aT29fGf/Xu.fx5AuFrCAxA7Im1Z/KaILD6w2cJAtxPRtsq.', 'messi', 'peminjam');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tb_anggota`
+-- Indeks untuk tabel `pinjaman`
+--
+ALTER TABLE `pinjaman`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_anggota`
 --
 ALTER TABLE `tb_anggota`
   ADD PRIMARY KEY (`id_anggota`);
 
 --
--- Indexes for table `tb_buku`
+-- Indeks untuk tabel `tb_buku`
 --
 ALTER TABLE `tb_buku`
   ADD PRIMARY KEY (`id_buku`);
 
 --
--- Indexes for table `tb_transaksi`
+-- Indeks untuk tabel `tb_transaksi`
 --
 ALTER TABLE `tb_transaksi`
   ADD PRIMARY KEY (`id_transaksi`);
 
 --
--- Indexes for table `tb_user`
+-- Indeks untuk tabel `tb_user`
 --
 ALTER TABLE `tb_user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `tb_anggota`
+-- AUTO_INCREMENT untuk tabel `pinjaman`
+--
+ALTER TABLE `pinjaman`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_anggota`
 --
 ALTER TABLE `tb_anggota`
   MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `tb_buku`
+-- AUTO_INCREMENT untuk tabel `tb_buku`
 --
 ALTER TABLE `tb_buku`
-  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
--- AUTO_INCREMENT for table `tb_transaksi`
+-- AUTO_INCREMENT untuk tabel `tb_transaksi`
 --
 ALTER TABLE `tb_transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT for table `tb_user`
+-- AUTO_INCREMENT untuk tabel `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
